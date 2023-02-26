554cc7618c147a851b003fca70d6acb38ee36dba
Trungtq2003 <trungtqph21525@fpt.edu.vn>
2023-02-26 18:57:02 +0700

    update

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 
 const app = document.querySelector("#app");
-import AboutPage from "./pages/about";
 import AdminProductAddPage from "./pages/admin/Product-add";
 import AdminProductEditPage from "./pages/admin/Product-edit";
 import AdminProductsPage from "./pages/admin/Products";
 import HomePage from "./pages/HomePage";
-import NotFoundPage from "./pages/NotFound";
 import { render, router } from "./utilities";
 
 router.on("/", () => render(HomePage, app));
